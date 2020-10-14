699831562745eb1c2a9a293c137e46f14ed4945c
POTCHONA Justin <potchjust@gmail.com>
2020-10-14 21:36:06 +0000

    :sparkles: tts
    done the offline version

--- main.py
+++ main.py
@@ -3,16 +3,24 @@ import gtts
 import datetime
 from playsound import playsound
 from utils import is_connected
+import pyttsx3
+
 
 
 def onlineConversion(input):
+    print("Veuillez patientez pendant la conversion")
     input = gtts.gTTS(input)
     ts = datetime.datetime.now().timestamp()
     input.save("output/"+str(ts)+'.mp3')
+    print("conversion terminé: la lecture debute :)")
     playsound("output/"+str(ts)+'.mp3')
 
 def offlineConversion(input):
-    print("from offline wip")
+    print("Veuillez patientez pendant la conversion")
+    engine = pyttsx3.init()
+    print("conversion terminé: la lecture debute :)")
+    engine.say(input)
+    engine.runAndWait()
 
 
 def main():
